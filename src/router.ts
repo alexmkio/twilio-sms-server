594c3c50ecf924f8c80d33c1131482b1d3f7077d
Alex Kio <alexandermartelkio@gmail.com>
2022-01-17 16:59:49 -0500

    Add typings

--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,31 @@
 import express, { Request, Response } from 'express'
 const router = express.Router()
 import session from 'express-session'
-// var session = require('express-session')
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-// var bodyParser = require('body-parser')
+const MessagingResponse = require('twilio').twiml.MessagingResponse
 import bodyParser from 'body-parser'
 
-router.use(session({secret: 'anything-you-want-but-keep-secret'}));
+router.use(session({secret: process.env.SESSION_SECRET as string}))
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
-router.post('/sms', (req, res) => {
+router.post('/sms', async (request: Request, response: Response) => {
+
   const twiml = new MessagingResponse()
   const message = twiml.message();
   
-  if (req.body.Body == 'hello') {
+  if (request.body.Body == 'hello') {
     message.body('Hi!')
     message.media('https://c.tenor.com/zQWHcFPU1-gAAAAC/forrest-gump.gif')
-  } else if (req.body.Body == 'bye') {
+  } else if (request.body.Body == 'bye') {
     message.body('Goodbye')
     message.media('https://media0.giphy.com/media/COYGe9rZvfiaQ/200.gif')
   } else {
-    message.body('I got nothing ¯\_(ツ)_/¯')
+    message.body('come again?')
     message.media('https://c.tenor.com/oRhDoibDP0kAAAAM/barack-obama-former-us-president.gif')
   }
 
-  res.writeHead(200, { 'Content-Type': 'text/xml' })
-  res.end(twiml.toString())
+  response.writeHead(200, { 'Content-Type': 'text/xml' })
+  response.end(twiml.toString())
 })
 
 export default router
