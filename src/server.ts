77ae4f635c7759435ca9a890b8c0e55a76c6f964
Alex Kio <alexandermartelkio@gmail.com>
2022-01-17 20:50:34 -0500

    Test without session

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
 import express from 'express'
 const app = express()
-import session from 'express-session'
+// import session from 'express-session'
 import * as dotenv from "dotenv"
 dotenv.config()
 import router from './router'
 
-if (process.env.NODE_ENV === 'production') {
-  app.set('trust proxy', 1)
-  app.use(session({
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-  }))
-} else {
-  app.use(session({
-    secret: process.env.SESSION_SECRET as string,
-    cookie: {}
-  }))
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.set('trust proxy', 1)
+//   app.use(session({
+//     secret: process.env.SESSION_SECRET as string,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: true }
+//   }))
+// } else {
+//   app.use(session({
+//     secret: process.env.SESSION_SECRET as string,
+//     cookie: {}
+//   }))
+// }
 
 const PORT: number = parseInt(process.env.PORT as string) || 1337
 
